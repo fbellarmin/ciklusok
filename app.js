19bbfacf683a4c0c916724f1cd954a4626623462
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-08 00:54:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,27 @@ for (triangleCounter = 0; triangleCounter < 30; triangleCounter++) {
     context.strokeStyle = 'grey';
     context.stroke();
 }
+
+//Ezt követő kódot vegyük ki a többi láthatóságához.
+var canvasWidth = canvas.width;
+var canvasHeight = canvas.height;
+
+var rectWidth = canvasWidth;
+var rectHeight = canvasHeight;
+
+var positionX = 0;
+var positionY = 0;
+
+var hue = 360;
+var saturation = 60;
+var lightness = 45;
+
+for (var rectCounter = 0; rectCounter < 17; rectCounter++) {
+    context.fillStyle = 'hsl(' + hue + ',' + saturation + '%,' + lightness + '%)';
+    context.fillRect(positionX, positionY, rectWidth, rectHeight);
+    hue -= 360 / 17;
+    positionX += 10;
+    positionY += 10;
+    rectWidth -= 20;
+    rectHeight -= 20;
+}
