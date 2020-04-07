38878f2a193ac6a691bd4f607080c66dbacbde26
Franyó Bellarmin <f.bellarmin93@gmail.com>
2020-04-08 00:48:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ for (rowCounter = 0; rowCounter < 5; rowCounter++) {
         context.fillRect(positionX + squareCounter * (size + padding), positionY + rowCounter * (size + padding), size, size);
     }
 }
+
+var size = 30;
+var positionY = 50;
+var padding = 5;
+
+var red = 255;
+var green = 79;
+var blue = 120;
+
+for (var rowCounter = 0; rowCounter < 6; rowCounter++) {
+    var positionX = 125;
+    for (var squareCounter = 0; squareCounter < 6; squareCounter++) {
+        context.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        context.fillRect(positionX, positionY, size, size);
+        red -= 7;
+        positionX += size + padding;
+    }
+    blue += 15;
+    positionY += size + padding;
+}
